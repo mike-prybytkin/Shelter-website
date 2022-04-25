2d0e8ba1fa243cd6d18004574088c333b81c6d03
Mike Prybytkin <prybytkin@bk.ru>
2022-04-26 02:37:11 +0300

    feat: add popup in html and css

---
+++ shelter/js/popup.js
@@ -0,0 +1,19 @@
+import PETS_LIST from './pets-list.js';
+const itemSlider = document.querySelectorAll('.item-slider');
+const body = document.querySelector('body');
+
+const timeOut = 500;
+
+const test = document.querySelector('.test');
+
+test.addEventListener('click', () => {
+    console.log('test')
+});
+
+alert(`
+Привет друг!
+Проверь плиз мою работу в среду =)
+Немного не успеваю допилить popup.
+Если что, можешь мне написать в дискорде, я всегда на связи =)
+Спасибо!
+`);
